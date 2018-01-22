85b9f21da457da05dd2c138779b74044393c5cde
steven <steve.luther@gmail.com>
2018-01-21 21:46:14 -0800

    Logging, flags for dryrun and no-cleanup.

--- README.md
+++ README.md
@@ -4,6 +4,11 @@ It's not the greatest thing ever, but it does what it's intended to do.
 
 I haven't tested this on files with spaces, and I'm guessing that's going to cause an issues.
 
+#### Requirements
+Make sure you have all the packages in the requirements.txt, `pip3 install requirements.txt`
+Make sure that the AWS CLI is installed `pip install awscli`
+Make sure that your AWS credentials are added to your ~/.aws directory. `aws configure`
+
 #### Usage
 Fairly straight forward, I modeled the syntax from the AWS CLI. This will take a file from the web, download it to the local /tmp directory, upload it to S3, and remove the temp file.
 ```bash
@@ -15,9 +20,10 @@ You can interchange the arguments, the order doesn't matter.
 - [ ] Check if the file exists and prompt for overwrite
 - [ ] Add logging
 - [ ] Tests
-- [ ] Add arg for status message, the current status tracker creates a lot of output if the file size is large
+- [X] Add arg for status message, the current status tracker creates a lot of output if the file size is large
 - [ ] Refactor some of this. It's a bit messy, but works for a first time go at it.
 - [ ] Use bucket filename instead of filename from web, if specified.
-- [ ] Flag to keep temp file.
+- [X] Flag to keep temp file.
 - [ ] Port this to AWS Lambda - Might run in to file size issues though, might be a whole new project using API Gateway, etc.
+- [ ] Bit of a stretch goal - Download entire directories. Might be difficult to tree
 
--- s3web.py
+++ s3web.py
@@ -14,6 +14,7 @@ Uses your AWS config file to upload.
 
 
 import os
+import logging
 import boto3
 import argparse
 import urllib.request
@@ -63,6 +64,7 @@ def get_file(url):
         f.write(file_buffer)
         # TODO: Add arg for status because this generates a ton of console output if the file size is large.
         status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+        print(status)
     return file_name
 
 
@@ -76,41 +78,116 @@ def boto_cp(temp_file, s3_bucket):
     return
 
 
+def remove_file(file_location):
+    os.remove(file_location)
+    return
+
+
 def main():
+    logging.basicConfig(filename='s3web.log',level=logging.DEBUG)
     helptext = '<S3 Bucket> <HTTP/S location> or <HTTP/S location> <S3 Bucket>'
-    parser = argparse.ArgumentParser(description='Get web URL and upload to S3 Bucket')
-    args = parser.add_argument('strings', type=str, nargs=2, help=helptext)
+    parser = argparse.ArgumentParser(description='Get web URL and upload to S3 Bucket',
+                                     epilog='Written by Steven Luther - 2018\n\n'
+                                            'For any issues or suggestions refer to Github\n'
+                                            'https://github.com/luthes/s3web\n')
+    parser.add_argument('strings',
+                        type=str,
+                        nargs=2,
+                        help=helptext)
+
+    # TODO: I don't think I need all of these bool args. One should be sufficient, and check True/False
+    parser.add_argument('--progress',
+                        dest='progress',
+                        action='store_true',
+                        help='Add progress message for downloading files, this is the default behavior.')
+    parser.add_argument('--no-progress',
+                        dest='progress',
+                        action='store_false',
+                        help='Remove progress message for downloading files.')
+    parser.set_defaults(status=True)
+    parser.add_argument('--cleanup',
+                        dest='cleanup',
+                        action='store_true',
+                        help='Delete temp files after files are upload to S3, this is the default behavior.')
+    parser.add_argument('--no-cleanup',
+                        dest='cleanup',
+                        action='store_false',
+                        help='Keep temp files after S3 upload.')
+    parser.set_defaults(cleanup=True)
+    parser.add_argument('--dryrun',
+                        dest='dryrun',
+                        action='store_false',
+                        help='Show file URL and S3 bucket files will be uploaded to/from')
+    parser.add_argument('--no-dryrun',
+                        dest='dryrun',
+                        action='store_true',
+                        help='Just upload the files...')
+    parser.set_defaults(dryrun=False)
     strings = (parser.parse_args()).strings
+    cleanup_arg = (parser.parse_args()).cleanup
+    progress_arg = (parser.parse_args()).progress
+    dryrun_arg = (parser.parse_args()).dryrun
+
+    # Logging initialization needs to go here so we can get a log level from arguments
+    logging.basicConfig(filename='s3web.log',
+                        level=logging.INFO,
+                        format='%(asctime)s %(message)s',
+                        datefmt='%m/%d/%Y %I:%M:%S %p')
+    # TODO Put these in a loop. I'm going to bed.
+    logging.info('Argument: Key - {0} - Value {1}'.format("Strings", strings))
+    logging.info('Argument: Key - {0} - Value {1}'.format("Cleanup", str(cleanup_arg)))
+    logging.info('Argument: Key - {0} - Value {1}'.format("Progress", str(progress_arg)))
+    logging.info('Argument: Key - {0} - Value {1}'.format("Dryrun", str(dryrun_arg)))
 
     if strings[0].startswith('s3'):
         s3_url = strings[0]
         http_url = strings[1]
+        logging.debug('First string is the S3 bucket {0}'.format(s3_url))
+        logging.debug('Second string is the web file {0}'.format(http_url))
     else:
         s3_url = strings[1]
         http_url = strings[0]
+        logging.debug('First string is the web file {0}'.format(http_url))
+        logging.debug('Second string is the S3 Bucket {0}'.format(s3_url))
 
     # Global temp file location
     temp_file_name = get_file_name(http_url)
     # Temp file location for cleanup
-    temp_file_location = '/tmp/' + get_file_name(http_url)
+    temp_file = '/tmp/' + get_file_name(http_url)
     # Global S3 File Location
     s3_path = str(s3_url + temp_file_name)
 
     # Download File from Web address
-    print('Getting file from web: ' + http_url)
-    get_file(http_url)
+    if dryrun_arg is False:
+        print('Getting file from web: ' + http_url)
+        get_file(http_url)
 
-    # Copy file to S3
-    print('Putting file to bucket: ' + s3_path)
+        # Copy file to S3
+        print('Putting file to bucket: ' + s3_path)
 
-    # TODO: Check if file exists in bucket, and prompt for overwrite, maybe add arg for auto overwrite
+        # TODO: Check if file exists in bucket, and prompt for overwrite, maybe add arg for auto overwrite
+        # S3_URL + FileName
+        logging.info('Starting transfer of files: {0} to {1}'.format(temp_file_name, s3_path))
+        boto_cp(temp_file_name, s3_path)
+        logging.info('Transfer of files complete')
 
-    # S3_URL + FileName
-    boto_cp(temp_file_name, s3_path)
-
-    # Clean up temp files
-    os.remove(temp_file_location)
-    print('Cleaning up temp file: ' + temp_file_location)
+        # Clean up temp files
+        if cleanup_arg:
+            logging.info('Cleanup set to True, removing temp file.')
+            print('Cleaning up temp file: ' + temp_file)
+            remove_file(temp_file)
+            logging.debug('Cleanup complete, removed {0}'.format(str(temp_file)))
+        else:
+            print('Skipping Cleanup')
+    else:
+        logging.debug('Dry Run Enabled - Nothing moved')
+        logging.debug('Getting file from web: ' + http_url)
+        logging.debug('Putting file to bucket: ' + s3_path)
+        logging.debug('Cleaning up temp file: ' + temp_file)
+        print('Dry Run Enabled - Nothing moved')
+        print('Getting file from web: ' + http_url)
+        print('Putting file to bucket: ' + s3_path)
+        print('Cleaning up temp file: ' + temp_file)
 
 
 if __name__ == '__main__':
